b7e88f792a6066e08b8f80539b10615169be55ec
Fredy Yecid Castro Agray <fredy.castro@gmail.com>
2025-07-31 06:50:39 -0500

    hook post

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,1 +1,16 @@
-print ("este es el post")
+#print("Bye Bye")
+#ejecuta comandos de git para inicializar el repo agregarlo todo y hacer un primer commit
+import os
+import subprocess
+
+MESSAGE_COLOR = "\x1b[34m"
+RESET_ALL = "\x1b[0m"
+
+print(f"{MESSAGE_COLOR}Almost done!")
+print(f"Initializing a git repository...{RESET_ALL}")
+
+subprocess.call(['git', 'init'])
+subprocess.call(['git', 'add', '*'])
+subprocess.call(['git', 'commit', '-m', 'Initial commit'])
+
+print(f"{MESSAGE_COLOR}The beginning of your destiny is defined now! Create and have fun!{RESET_ALL}")
